6cfa123d03e0624660bf0e34ba577b5749c62502
Andrey <82231196+Badebum@users.noreply.github.com>
2021-11-05 11:46:24 +0300

    Готовая логика заметок

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
 import TodoList from './components/TodoList';
-
+import TodoEditor from './components/TodoEditor';
 class App extends Component {
   state = {
     todos: [
@@ -11,11 +12,41 @@ class App extends Component {
     ],
   };
 
+  addTodo = text => {
+    console.log(text);
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({todos}) => ({
+      todos: [todo, ...todos],
+    }));
+  };
+
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
   };
+
+  toggleCompleted = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === todoId) {
+          console.log('пиши тодо');
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      }),
+    }));
+  };
+
   render() {
     const { todos } = this.state;
 
@@ -26,11 +57,16 @@ class App extends Component {
     );
     return (
       <>
+        <TodoEditor onSubmit={this.addTodo} />
         <div>
           <p>Общее количество: {totalTodoCount}</p>
           <p>Выполнено: {completedTodoCount}</p>
         </div>
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </>
     );
   }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './TodoList.module.css';
 
-const TodoList = ({ todos, onDeleteTodo }) => (
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
   <ul>
-    {todos.map(({ id, text }) => (
+    {todos.map(({ id, text, completed }) => (
       <li key={id} className={styles.list}>
+        <input
+          type="checkbox"
+          className={'TodoList__checkbox'}
+          checked={completed}
+          onChange={() => onToggleCompleted(id)}
+        />
+
         <p>{text}</p>
-        <button type="button" className={styles.button} onClick={()=> onDeleteTodo(id)}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => onDeleteTodo(id)}
+        >
           delete
         </button>
       </li>
